c076c82d237482707ee9485b2b840468ab5c5671
GB <GB@GBs-MacBook-Pro.cable.rcn.com>
2015-10-18 17:31:00 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-display_board  {puts "
+def display_board
 
+"
    |   |
 -----------
    |   |
 -----------
-   |   |"}
+   |   |"
+end
