5475467b03f8274ca1e9ab85fbc80674d22b06f6
github username <github email address>
2015-10-28 04:25:08 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,16 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+def display_board
+  row = "   |   |   "
+  dash = "-" * 11
+
+  2.times do 
+    puts row
+    puts dash
+  end
+  puts row
+end
+
+
+display_board
+
