f507629a6fac0c1e88b51c8f1df72cab6ac5ef23
dunxtand <mdrstandish@gmail.com>
2015-11-04 14:30:36 -0500

    Done

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+def display_board
+  row = "   |   |   "
+  separator = "-----------"
+  (1..5).each do |n|
+    if n % 2 != 0
+      puts row
+    else
+      puts separator
+    end
+  end
+end
