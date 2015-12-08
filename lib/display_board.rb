58278a183d16cc1b3a3314d7e25cfcad319aa64b
Jurtly <jurtly@icloud.com>
2015-12-08 15:46:15 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,13 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  board = "   |   |  "
+  underline = "____________"
+
+  puts board
+  puts underline
+  puts board
+  puts underline
+  puts board
+end
+
+display_board
+
