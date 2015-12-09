8403a9401f3535784d1203822deae3a8d62fb508
Jurtly <jurtly@icloud.com>
2015-12-09 14:20:14 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,11 @@
 def display_board
-  board = "   |   |  "
-  underline = "____________"
+ puts "   |   |"
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 
-  puts board
-  puts underline
-  puts board
-  puts underline
-  puts board
+
 end
 
 display_board
