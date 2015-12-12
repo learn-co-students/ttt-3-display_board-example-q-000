8ed1a5c192fedf83caf46f888aace6870d75cef4
bubbaxen <rbind71@yahoo.com>
2015-12-12 14:31:51 +0000

    nitrous-backup at Sat Dec 12 14:31:51 UTC 2015

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,4 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-
-
 def display_board
 board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 puts  "#{board[0]}|#{board[1]}|#{board[2]}"
@@ -9,4 +7,3 @@ puts  "#{board[3]}|#{board[4]}|#{board[5]}"
 puts  "-----------"
 puts  "#{board[7]}|#{board[8]}|#{board[9]}"
 end
-
