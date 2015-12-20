f1368edb1e8c237c4c14c929d404b316be29d599
Chen Yang <yangc5@mail.uc.edu>
2015-12-20 18:07:37 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  row1=["   ", "   ", "   "]
+  row2=["   ", "   ", "   "]
+  row3=["   ", "   ", "   "]
+  puts (row1[0]+"|"+row1[1]+"|"+row1[2])
+  puts "-----------"
+  puts (row2[0]+"|"+row2[1]+"|"+row2[2])
+  puts "-----------"
+  puts (row3[0]+"|"+row3[1]+"|"+row3[2])
+end
