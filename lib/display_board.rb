d1825a3fbc4a35e5d839aec8a6a0a3d57f8afc8a
JonatwinJ <jonatwinj@gmail.com>
2015-12-21 19:14:30 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,7 @@
 def display_board
-boardRow1 = [" ","|"," ","|"," "]
-boardRow2 = ["-----------"]
-boardRow3 = [" ","|"," ","|"," "]
-boardRow4 = ["-----------"]
-boardRow5 = [" ","|"," ","|"," "]
-
-puts boardRow1
-puts boardRow2
-puts boardRow3
-puts boardRow4
-puts boardRow5
-end
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end 
