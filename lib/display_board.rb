30772b59a48f4aab9c1c3dd9bf7175975b837ccf
JonatwinJ <jonatwinj@gmail.com>
2015-12-21 14:49:50 +0000

    nitrous-backup at Mon Dec 21 14:49:50 UTC 2015

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,13 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+boardRow1 = [" ","|"," ","|"," "]
+boardRow2 = ["-----------"]
+boardRow3 = [" ","|"," ","|"," "]
+boardRow4 = ["-----------"]
+boardRow5 = [" ","|"," ","|"," "]
+
+puts boardRow1
+puts boardRow2
+puts boardRow3
+puts boardRow4
+puts boardRow5
+end
