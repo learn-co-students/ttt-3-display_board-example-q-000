9c6ec8740970b74759de3ea2b5ead83e377448e6
wolfsandle <wolfsandle@gmail.com>
2016-01-14 14:45:43 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+#  1. Each cell is presented by a string with 3 spaces: <pre>"   "</pre>
+#  2. Each row has 3 cells, the middle separated by 2 `|` (pipe) characters: <pre>   |   |   </pre>
+#  3. There are 3 rows, with 2 separating lines of 11 `-` (dash) characters: `-----------`
+
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
