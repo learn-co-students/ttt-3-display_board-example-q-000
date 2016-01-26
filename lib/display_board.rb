3ad087ceda01da22fc5ade34b8881350b054936c
Azkm18111030302 <arunhatesyouall@gmail.com>
2016-01-26 01:56:46 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,17 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def row_values
+  return "   |   |   "
+end
+def end_row
+  return "-----------"
+end
+def display_board
+  board=[]
+  board.unshift(row_values)
+  board.unshift(end_row)
+  board.unshift(row_values)
+  board.unshift(end_row)
+  board.unshift(row_values)
+  puts board
+end
+display_board
