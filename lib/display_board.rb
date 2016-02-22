d8a8956fa972ac64784d2822d4836e5a646ed3e4
jamesmccarthy65 <jamesmccarthy65@gmail.com>
2016-02-22 21:12:06 +0000

    nitrous-backup at Mon Feb 22 21:12:05 UTC 2016

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board
   output_line1 = "   " + "|" + "   " + "|" + "   "
   output_line2 = "-----------"
-  output_line3 = "   " + "|" + "   " + "|" + "   "
+  output_line3 = "   " + "|" + "   " + "|" + "  "
   output_line4 = "-----------"
-  output_line5 = "   " + "|" + "   " + "|" + "   "
-  puts (output_line1 + "\n" + output_line2 + "\n" + output_line3 +"\n"+ output_line4 + "\n" + output_line5 )
+  output_line5 = "   " + "|" + "   " + "|" + "  "
+puts (output_line1 + "\n" + output_line2 + "\n" + output_line3 +"\n"+ output_line4 + "\n" + output_line5 )
 end
 display_board
