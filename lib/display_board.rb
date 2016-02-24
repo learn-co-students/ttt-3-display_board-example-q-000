3b32c8e324dd5bad02d5574712ca2a3788ba5425
Cauliflowrr <ceeceebeme@gmail.com>
2016-02-24 01:21:42 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 def display_board
-  puts    |   |
-  puts ___________
-  puts    |   |
-  puts ___________
-  puts    |   |
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
