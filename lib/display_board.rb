038634289905d5b334f21f0ceb6caa8a0c889e6d
Valentin Rosu <alispace410@gmail.com>
2016-02-29 19:51:50 +0000

    nitrous-backup at Mon Feb 29 19:51:50 UTC 2016

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,3 +2,12 @@
 def display_board
  puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
 end
+
+def display_rainbow(col)
+  puts "R: #{col[0]}, O: #{col[1]}, Y: #{col[2]}, G: #{col[3]}, B: #{col[4]}, I: #{col[5]}, V: #{col[6]\n}"
+end
+
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+display_rainbow(colors)
+
