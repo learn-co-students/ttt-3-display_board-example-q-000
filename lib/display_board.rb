907618a6b8445fc465e7689b664a62b1be764333
jwdinker <jwdinker.accounts@gmail.com>
2016-03-25 21:28:15 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,14 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+
+def display_board
+  1.upto(5) do |lineNumber|
+    if lineNumber % 2 == 0
+       puts "-----------"
+    else
+      puts "   |   |   "
+    end
+  end
+end
+
+ display_board
