d5f9d9df87d03e9f51ab150f23e5c9b9ba86ee95
IantheH <ianthehernandez@gmail.com>
2016-04-05 19:34:03 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,10 @@
 def display_board
-  board = [" "," "," "," "," "," "," "," "," "]
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
-  display_board(board)
-
 
 
 
