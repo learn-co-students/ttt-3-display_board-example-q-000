514621a6743b4584cdaa14d6ec7fc3791fefc4b2
Annette Mili Kemppi <annette.kemppi@helsinki.fi>
2016-04-19 17:22:05 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,8 @@
 def display_board
- puts    |   |
- puts -----------
- puts    |   |
- puts -----------
- puts    |   |
-end# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
+end
+# Define a method display_board that prints a 3x3 Tic Tac Toe Board
