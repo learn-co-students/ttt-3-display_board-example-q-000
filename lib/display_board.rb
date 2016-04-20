f9f86d1b33f95f04fe00bf826ce2de7b8158f5e9
KnKSprinter <akokoz89@gmail.com>
2016-04-20 17:55:38 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,15 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+
+def display_board
+  board = Array.new(9, " ")
+  divider = "-----------"
+  temp = board.map{|b| " #{b} "}
+  puts temp[0..2]*"|"
+  puts divider
+  puts temp[3..5]*"|"
+  puts divider
+  puts temp[6..8]*"|"
+end
+
+display_board
