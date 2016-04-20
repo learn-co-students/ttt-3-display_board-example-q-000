bf0072f6bc832ac88e74e91c0911263921615b7c
jm-rives <joanna.rives@gmail.com>
2016-04-20 14:56:21 -0700

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,17 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+# stub developed by Joanna M. Rives @ jm_rives
+# from the Flat Iron School's Learn.co Intro to Ruby
+
+def display_board
+  bar_vert = "   |   |   \n"
+  bar_horz = "-----------"
+ 
+  # board construction
+  puts bar_vert
+  puts bar_horz
+  puts bar_vert
+  puts bar_horz
+  puts bar_vert
+end
+
+display_board
