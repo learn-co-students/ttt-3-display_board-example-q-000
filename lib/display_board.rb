4005f565f9d4555d54c8b1a8da0f6572edb5c420
thedailyephemera <thedailyephemera@gmail.com>
2016-04-25 13:13:49 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+	row = []
+	3.times do
+		row << "   "
+	end
+	separator = "-" * 11
+	puts row.join("|") + "\n" + separator + "\n" + row.join("|") + "\n" + separator + "\n" + row.join("|")
+end
+
+display_board
