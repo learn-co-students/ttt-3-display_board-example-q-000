ec9f581f88b15d597d2c64b87b1ef16464229e2c
JBeni <jitca.beniamin@gmail.com>
2016-04-25 08:33:20 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,23 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+puts "Welcome to Tic Tac Toe"
+
+def display_board
+
+  board = [" "," "," "," "," "," "," "," "," "]
+
+  puts " #{board[1]} | #{board[2]} | #{board[3]} "
+  puts "-----------"
+  puts " #{board[4]} | #{board[5]} | #{board[6]} "
+  puts "-----------"
+  puts " #{board[7]} | #{board[8]} | #{board[9]} "
+  
+  puts "What move would X like to make (1-9)?"
+  
+  
+  
+  puts "What move would O like to make (1-9)?"
+  
+end
+
+  display_board
