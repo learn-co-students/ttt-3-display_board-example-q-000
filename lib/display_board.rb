e380f0170b87c2540a193e3c19f830f03aca8c01
JBeni <jitca.beniamin@gmail.com>
2016-04-25 08:41:49 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,20 +4,12 @@ puts "Welcome to Tic Tac Toe"
 
 def display_board
 
-  board = [" "," "," "," "," "," "," "," "," "]
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 
-  puts " #{board[1]} | #{board[2]} | #{board[3]} "
-  puts "-----------"
-  puts " #{board[4]} | #{board[5]} | #{board[6]} "
-  puts "-----------"
-  puts " #{board[7]} | #{board[8]} | #{board[9]} "
-  
-  puts "What move would X like to make (1-9)?"
-  
-  
-  
-  puts "What move would O like to make (1-9)?"
-  
 end
 
-  display_board
+  
