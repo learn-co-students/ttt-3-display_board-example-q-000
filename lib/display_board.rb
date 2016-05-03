d336bb4fcad899737b73c0aa1dbe288d18d93370
Alex Kokozidis <alexandros.kokozidis@gmail.com>
2016-05-03 19:11:32 +0300

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  board = Array.new(9, " ")
+  devider = "-----------"
+  board.map! {|b| " #{b} "}
+  puts board[0..2] * "|"
+  puts devider
+  puts board[3..5] * "|"
+  puts devider
+  puts board[6..8] * "|"
+end
+display_board
