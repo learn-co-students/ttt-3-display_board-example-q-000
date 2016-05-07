d84f138d5ecdf147d676e20ce906e014b70c4da9
HerptyDerpoty <herptyderpoty@gmail.com>
2016-05-07 21:45:17 +0000

    nitrous-backup at Sat May  7 21:45:17 UTC 2016

--- h.rb
+++ h.rb
@@ -1,4 +1,8 @@
-def display_board
-  " | | "
+def display_board(q,w,e,r,t,y,u,i,o)
+  puts " #{q} | #{e} | #{r} "
+  puts "-----------"
+  puts " #{t} | #{y} | #{u}  "
+  puts "-----------"
+  puts " #{u} | #{i} | #{o} "
 end
-display_board
+
