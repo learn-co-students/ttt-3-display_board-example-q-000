19ca54111d33b6086ce1ffd501d3c36ac611812d
Josh Hirshfeld <josh.hirshfeld@flatironschool.com>
2016-05-25 12:51:52 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
+require 'pry'
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  foo = 1
+   puts "   |   |   "
+   puts "-----------"
+   puts "   |   |   "
+   puts "-----------"
+   puts "   |   |   "
+end
+
+display_board
