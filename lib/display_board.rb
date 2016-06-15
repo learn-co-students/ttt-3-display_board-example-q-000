359fa1749b0a54a2b9c8b2d5bd7f185988919671
Eduardo Daroza <ed_daroza@hotmail.com>
2016-06-15 05:13:10 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  row = "   |   |   "
+  separation = "-----------"
+  puts row
+  puts separation
+  puts row
+  puts separation
+  puts row
+end
+display_board
