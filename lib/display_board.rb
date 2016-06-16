db62db4d3b235bf61e9ba371760562dfd0b9e3e9
paige11 <ellis.paige.v@gmail.com>
2016-06-16 10:38:25 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,5 @@
 def display_board
-  row1 = ["   |   |   "]
-  line1 = ["-----------"]
-  row2 = ["   |   |   "]
-  line2 = ["-----------"]
-  row3 = ["   |   |   "]
-  puts row1 + line1 + row2 + line2 + row3
+  row = ["   |   |   "]
+  line = ["-----------"]
+  puts row + line + row + line + row
 end
