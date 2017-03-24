ff3693aa3d03620481e5ed196b6809a096f28498
Sam Marshall <samuel.r.marshall@gmail.com>
2017-03-24 00:33:06 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,16 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def odd_row
+  puts "   |   |   "
+end
+def even_row
+  puts "-----------"
+end
+
+def display_board
+  print odd_row
+  print even_row
+  print odd_row
+  print even_row
+  print odd_row
+end
+print display_board
